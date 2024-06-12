154e1cd3982c64384f287f3a844d6026eef577d3
jourdannf <jourdann.fraser@gmail.com>
2024-06-11 21:35:44 -0400

    Added comments and included additional photos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 const idols = [
     {name:"Choi Aim", link:"./images/choi_aim.jpg"}, 
-    {name:"Hwang Seyoung", link: "./images/hwangseyoung.jpeg"},
-    {name:"Son Yeongseo", link:"./images/choi_aim.jpg"},
-    {name:"Jung Yuri", link: "./images/choi_aim.jpg"},
-    {name: "Reta", link: "./images/choi_aim.jpg"},
-    {name: "Kang Yunjeong", link: "./images/choi_aim.jpg"}
+    {name:"Hwang Seyoung", link: "./images/hwang_seyoung.jpg"},
+    {name:"Son Yeongseo", link:"./images/son_yeongseo.jpg"},
+    {name:"Jung Yuri", link: "./images/jung_yuri.jpg"},
+    {name: "Reta", link: "./images/reta.jpg"},
+    {name: "Kang Yunjeong", link: "./images/kang_yunjeong.jpg"},
+    {name: "Lee Arumsoul", link: "./images/lee_arumsoul.jpg"},
+    {name: "Khan Amina", link: "./images/khan_amina.jpg"},
+    {name: "Yoon Minseo", link: "./images/yoon_minseo.jpg"}
 ];
 const finalIdols = [];
-console.log(document.querySelector("form").lastElementChild)
 
-const yourPicks = document.querySelector("form").insertBefore(document.createElement("div"), document.querySelector("form").lastElementChild);
-// const yourPicks = document.querySelector("form").appendChild(document.createElement("div"));
+const yourPicks = document.querySelector("form").insertBefore(document.createElement("div"), document.querySelector("form").lastElementChild);//query selector and child to navigate elements
 yourPicks.id = "yourPicks";
 yourPicks.style.width = "900px";
 yourPicks.classList.add("row", "mx-auto", "d-flex", "justify-content-center");
@@ -28,7 +29,7 @@ function createIdolTemplate (name, link){
     profilePic.classList.add("card-img-top");
     const title = card.appendChild(document.createElement("div"));
     title.classList.add("card-body", "text-center");
-    title.textContent = name;
+    title.textContent = name; //Modify text content
 
     return frag;
 
@@ -59,7 +60,7 @@ document.getElementById("contestantsBox").classList.add("mb-3");
 //Add event listener; when you click on card, it's highlighted
 
 const contestants = document.getElementById("contestantsBox").firstChild.childNodes;
-document.getElementById("contestantsBox").addEventListener("click", handleContestantClick);
+document.getElementById("contestantsBox").addEventListener("click", handleContestantClick); //First event listener
 
 function handleContestantClick(e){
     let idolName = "";
@@ -78,7 +79,7 @@ function handleContestantClick(e){
     
     //Add or remove from the final list collection
     if (finalIdols.includes(idolName)){
-        e.target.parentNode.classList.remove("border-dark");
+        e.target.parentNode.classList.remove("border-dark"); //parentNode to navigate element
         finalIdols.splice(finalIdols.indexOf(idolName), 1);
         yourPicks.childNodes.forEach((pick)=>{ //Iterate over collection of elements
             if (e.target.src === pick.src){
@@ -92,7 +93,7 @@ function handleContestantClick(e){
         }
 
         //Add border to card
-        e.target.parentNode.classList.add("border-dark");
+        e.target.parentNode.classList.add("border-dark"); //classes and style are both attributes so I'm counting them for both here
         e.target.parentNode.style.backgroundColor = "rgb(238, 238, 238)";
 
         finalIdols.push(idolName);
@@ -100,17 +101,15 @@ function handleContestantClick(e){
         yourPicks.appendChild(createPicksTemplate(e.target.src));
         e.target.classList.add("addedPick");
     }
-
-    //You confirm that final list with window.confirm when they hit submit.
 }
 
 //SUBMISSION VALIDATION
 
 const form = document.getElementById("form-container");
-form.addEventListener("submit", checkSelection);
+form.addEventListener("submit", checkSelection); //Second Event Listener
 const ggName = form.elements["groupName"];
 
-function checkSelection(e){ //This is the form validation based on DOM event
+function checkSelection(e){ //Form validation based on DOM event
     const regex = /^(?!.*\W).*/;
 
     console.log(regex.test(ggName));
@@ -122,15 +121,14 @@ function checkSelection(e){ //This is the form validation based on DOM event
 
     if (finalIdols.length < 5){
         e.preventDefault();
-        window.alert("You need to pick exactly five people in order to create a complete group.");
+        window.alert("You need to pick exactly five people in order to create a complete group."); //BOM Method 1
         return;
     }
 
+    //BOM Method 2
     const ready = window.confirm("Are you sure you're okay with " + finalIdols[0] + ", " + finalIdols[1] + ", " + finalIdols[2] + ", " +finalIdols[3] + ", and " + finalIdols[4] + " as your picks for " + ggName.value + "?");
 
     if (!ready){
         e.preventDefault();
-    }else {
-        //Display sucess message
     }
 }
